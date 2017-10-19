ce05ce4480b4fb01853b1e8995858cd3614cc7c2
FaizalSanyo <faizal.sanyo@gmail.com>
2017-10-19 14:17:40 +0200

    added switch case

--- FlowControl2/FlowControl2/Program.cs
+++ FlowControl2/FlowControl2/Program.cs
@@ -10,6 +10,36 @@ namespace FlowControl2
     {
         static void Main(string[] args)
         {
+            MainMenu();
         }
+
+        public static void MainMenu()
+        {
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                Console.Clear();
+                Console.WriteLine("This is the main menu");
+                Console.WriteLine("Input a number to choose a funcion");
+                Console.WriteLine("0) Exit");
+
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        keepGoing = true;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input\nTry again ");
+                        Console.ReadLine();
+                        break;
+
+                }
+
+            }
+        }
+
+
+
     }
 }
