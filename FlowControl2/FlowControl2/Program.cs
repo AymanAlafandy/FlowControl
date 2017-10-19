fde6584f964ddb45516b65bb0439a922ffb0bb7f
bhanu18v <bhanu18v@gmail.com>
2017-10-19 14:55:16 +0200

    addedfunction

--- FlowControl2/FlowControl2/Program.cs
+++ FlowControl2/FlowControl2/Program.cs
@@ -21,11 +21,15 @@ namespace FlowControl2
                 Console.WriteLine("This is the main menu");
                 Console.WriteLine("Input a number to choose function");
                 Console.WriteLine("0) Exit");
+                Console.WriteLine("Cinema");
                 string input = Console.ReadLine();
                 switch (input)
                 {
                     case "0":
                         keepGoing = false;
+                        break;
+                    case "1":
+                        AdolescentOrSenior();
                         break;
                     default:
                         Console.WriteLine("Incorrect input\nTry Again");
@@ -40,6 +44,22 @@ namespace FlowControl2
 
         public static void AdolescentOrSenior()
         {
+            Console.WriteLine("What is your Age?:");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
+            if (age < 20)
+            {
+                Console.WriteLine("Kids Deal:80kr");
+            }
+            else if (age > 64)
+            {
+                Console.WriteLine("Senior Citizen Discount:90kr");
+            }
+            else
+            {
+                Console.WriteLine("Standard Price: 120kr");
+            }
+            Console.ReadLine();
 
         }
     }
