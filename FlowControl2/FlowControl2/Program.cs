0ff20f2d45040d273ef2f45ef4c70e1f7ac3852f
huongngt <huongnt.x86@gmail.com>
2017-10-19 14:55:24 +0200

    AdolescentOrSenior completed

--- FlowControl2/FlowControl2/Program.cs
+++ FlowControl2/FlowControl2/Program.cs
@@ -22,13 +22,16 @@ namespace FlowControl2
                 Console.WriteLine("This is the main menu.");
                 Console.WriteLine("Input number to choose function");
                 Console.WriteLine("0) Exit");
+                Console.WriteLine("1) Cinema");
                 string input = Console.ReadLine();
                 switch (input)
                 {
                     case "0":
                         keepGoing = false;
                         break;
-                    
+                    case "1":
+                        AdolescentOrSenior();
+                        break;
                     default:
                         Console.WriteLine("Incorrect choice \nPlease input again");
                         Console.ReadKey();
@@ -40,7 +43,24 @@ namespace FlowControl2
 
         public static void AdolescentOrSenior()
         {
+            Console.Clear();
+            Console.Write("What is your age:");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
 
+            if (age < 20)
+            {
+                Console.WriteLine("Kid deals: 80kr");
+            }
+            else if(age > 64)
+            {
+                Console.WriteLine("Senior Citizen Discount: 90kr");
+            }
+            else
+            {
+                Console.WriteLine("Standard Price: 120kr");
+            }
+            Console.ReadKey();
         }
     }
 }
