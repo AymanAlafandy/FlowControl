f7ec69106e57f58adbad6d5ec85539400c9ec14f
FaizalSanyo <faizal.sanyo@gmail.com>
2017-10-19 14:57:14 +0200

    Added Cinema Menu and Ticket Deal

--- FlowControl2/FlowControl2/Program.cs
+++ FlowControl2/FlowControl2/Program.cs
@@ -28,11 +28,16 @@ namespace FlowControl2
                 Console.Clear();
                 Console.WriteLine("This is the main menu");
                 Console.WriteLine("Input a number to choose a funcion");
+                Console.WriteLine("1) Cinema");
                 Console.WriteLine("0) Exit");
 
                 string input = Console.ReadLine();
-                switch (input)
+
+                switch (input) // Switch-case belongs to while
                 {
+                    case "1":
+                        AdolescentOrSenior();
+                        break;
                     case "0":
                         keepGoing = true;
                         break;
@@ -43,7 +48,26 @@ namespace FlowControl2
                 }
             }
         }
+        public static void AdolescentOrSenior()
+        {
+            Console.WriteLine("What is your age?: ");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
 
+            if (age < 20)
+            {
+                Console.WriteLine("Kids deal : 80kr");
+            }
+            else if (age > 64)
+            {
+                Console.WriteLine("Senior Citizen Discount : 90kr");
+            }
+            else
+            {
+                Console.WriteLine("Standard Price : 120kr");
+            }
+            Console.ReadLine();
+        }
      
 
     }
