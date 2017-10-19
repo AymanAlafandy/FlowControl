2947baa1e0b7bd029658b0032c1ba6d54b087b7b
nicolepintea <nicole.pintea@gmail.com>
2017-10-19 15:08:37 +0200

    Repeat Ten Times Done

--- FlowControl2/FlowControl2/Program.cs
+++ FlowControl2/FlowControl2/Program.cs
@@ -20,21 +20,54 @@ namespace FlowControl2
                 Console.Clear();
                 Console.WriteLine("This is the main menu.");
                 Console.WriteLine(" Input a number to chooce function");
+                Console.WriteLine("1) Cinema");
+                Console.WriteLine("2) Repeat");
                 Console.WriteLine("0) Exit");
                 string input = Console.ReadLine();
                 switch(input)
                 {
+                    case "1":
+                        AdolescentOrSenior();
+                        break;
                     case "0":
                         keepGoing = false;
                         break;
                     default:
-                        Console.WriteLine("Incorerrect input¨|Try again!");
-                    Console.ReadKey();
-                    break;
+                        Console.WriteLine("Incorrect input\nTry again!");
+                        Console.ReadKey();
+                        break;
                 }
             }
 
         }
         public static void AdolescentOrSenior()
+        {
+            Console.Write("What is your age?: ");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
+            if (age<20)
+            {
+                Console.WriteLine("Kid Deals: 80 Kr");
+            }
+            else if (age>64)
+            {
+                Console.WriteLine("Senior Citizen Discount: 90Kr");
+            }
+            else
+            {
+                Console.WriteLine("Standard Price: 120Kr");
+            }
+            Console.ReadLine();
+        }
+        public static void RepeatTenTimes()
+        {
+            Console.WriteLine("What do you want to repeat? ");
+            string input = Console.ReadLine();
+            for (int i= 0; i< 10; i++)
+            {
+                Console.Write((i+1)+"."+input+", ");
+            }
+            Console.ReadLine();
+        }
     }
 }
