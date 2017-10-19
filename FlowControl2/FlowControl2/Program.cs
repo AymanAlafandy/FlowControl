5b08988a1a609e415c8f175bcdde87c3652e7675
John Hellman <acoppero@gmail.com>
2017-10-19 14:32:08 +0200

    Cleaning

--- FlowControl2/FlowControl2/Program.cs
+++ FlowControl2/FlowControl2/Program.cs
@@ -10,39 +10,7 @@ namespace FlowControl2
     {
         static void Main(string[] args)
         {
-            MainMenu();
         }
-        public static void MainMenu()
-        {
-            bool KeepGoing = true;
-            while (KeepGoing)
-            {
-                Console.clean();
-                Console.WriteLine("this is the menu.");
-                Console.WriteLine("input a num to chose function");
-                Console.WriteLine("0)Exite");
-                string input = Console.ReadLine();
-                switch (input)
-                {
-                    case "0":
-                        KeepGoing = false;
-                        break;
-                    default:
-                        Console.WriteLine("incorrect input\ntry again");
-                        Console.ReadKey();
-                        break;
-                }
-            }
-        }
-
-        public static void AdolescentOrSenior()
-        {
-
-        }
-
-        public static void MainMenu()
-        {
-
-        }
+      
     }
 }
