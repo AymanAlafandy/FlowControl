edb689a11fa49c6c06a8924a456179c54d8a2c91
Taleb01 <taleb_n1@hotmail.com>
2017-10-19 14:14:48 +0200

    First commit (just a try)

--- FlowControl2/FlowControl2/Program.cs
+++ FlowControl2/FlowControl2/Program.cs
@@ -8,8 +8,40 @@ namespace FlowControl2
 {
     class Program
     {
+        //everything is writting in the mainMethod will be in the memory because it is static
+        //so it is recommanded to keep it clean or write small code on it.
         static void Main(string[] args)
         {
+            MainMenu();
+        }
+
+        public static void MainMenu()
+        {
+            bool KeepGoing = true;
+            while (KeepGoing)
+            {
+                Console.Clear();
+                Console.WriteLine("This is the MainMenu.");
+                Console.WriteLine("Input number to choose function.");
+                Console.WriteLine("0) Exit");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        KeepGoing = false;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input\n Try Again \t If you want");
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
+        }
+
+        public static void AdolescentOrSenior()
+        {
+
         }
     }
 }
