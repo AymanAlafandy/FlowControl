2ef9616a3c0942d39216b5d250a4e8129ec48bc9
sunisakc <sunisa_kan@gmail.com>
2017-10-19 14:14:25 +0200

    Main Menu Shell Done

--- FlowControl2/FlowControl2/Program.cs
+++ FlowControl2/FlowControl2/Program.cs
@@ -10,6 +10,36 @@ namespace FlowControl2
     {
         static void Main(string[] args)
         {
+                 MainMenu();
         }
-    }
-}
+
+        public static void MainMenu()
+        {
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                Console.Clear();
+                Console.WriteLine("This is the main menu");
+                Console.WriteLine("Input of number to choose function");
+                Console.WriteLine("0) Exit");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        keepGoing = false;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input\nTry again");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+                public static void AdolescentOrSenior()
+              {
+
+               }
+            }
+        }
+    
